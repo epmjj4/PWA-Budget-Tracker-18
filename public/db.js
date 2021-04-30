80816d9e0b6106d2bfa02e39878e0497fcc79798
Nes <epmjj4@hotmail.com>
2021-04-30 14:35:53 -0400

    updated db.js file

--- public/db.js
+++ public/db.js
@@ -5,4 +5,9 @@ const request = indexedDB.open('budget', 1);
 
 reuqest.onupgradeneeded = function (event) {
     // this will create an object called pending
+    const db = event.target.result:
+    // this will check if app is online before reading from db
+    if (navigator.online) {
+        checkDatabase();
+    }
 }
